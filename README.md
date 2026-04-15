a3b38bc22d0338356a0c00404e2f9dc9f10a49f3
anandsrout07-glitch <anandsrout07@gmail.com>
2026-04-15 23:41:33 +0530

    Full code completed and updated in personal_info.py,test_input.txt file created,.gitignore file created,README.md file updated with sample output

--- README.md
+++ README.md
@@ -23,3 +23,37 @@ This is my first Python project! It's a program that stores and displays persona
 - Basic input validation
 - Age calculation in months
 
+## Sample Output
+======================================== 
+Enter Your Personal Information
+========================================
+
+Enter Your Name: Anand Sagar Rout
+Enter Your Age: 17
+Enter your city name: Sambalpur
+Enter your hobby: Playing Cricket
+======================================== 
+Tell me about your self
+========================================
+What's Your Favorite food: Biriyani
+What's your favorite color: Black 
+============================== 
+Your Personal Information      
+==============================
+Name:Anand Sagar Rout
+Age:17
+Hobby:Playing Cricket
+City:Sambalpur
+
+Favorite Food:
+Biriyani
+Favorite Color:
+Black
+
+========================================
+Thanks for using this programm
+========================================
+
+## Challenges & Solutions
+- Challenge: I didn't know how to handle empty input
+  Solution: Used a while loop to keep asking
--- personal_info.py
+++ personal_info.py
@@ -1,3 +1,43 @@
 #Personal Informaion Manger
 #My 1st week task(Anand Sagar Rout)
-print("Hello_World")
+
+#Variables for personal information
+print("="*40,"\nEnter Your Personal Information\n","="*40)
+name=input("\nEnter Your Name: ")
+age=int(input("Enter Your Age: "))
+city=input("Enter your city name: ")
+hobby=input("Enter your hobby: ")
+
+#User input for more information
+print("="*40,"\nTell me about your self\n","="*40)
+favorite_food=input("What's Your Favorite food: ")
+while favorite_food=="":
+    print("Please Enter Valid Food!")
+    favorite_food=input("What's Your Favorite food: ")
+
+favorite_color=input("What's your favorite color: ")
+while favorite_color=="":
+    print("Please Enter Valid color: ")
+    favorite_color=("What's your favorite color: ")
+
+
+#Calculating age in months
+age_in_months=age*12
+
+#Display Personal INFORMATION
+print("="*30,"\nYour Personal Information\n","="*30)
+
+print(f"Name:{name}")
+print(f"Age:{age}")
+print(f"Hobby:{hobby}")
+print(f"City:{city}")
+
+print(f"\nFavorite Food:\n{favorite_food}")
+print(f"Favorite Color:\n{favorite_color}")
+print()
+
+print("="*40,"\nThanks for using this programm\n","="*40)
+
+
+
+
